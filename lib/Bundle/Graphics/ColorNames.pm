080c6dc7c8f8350f38d1f988deca27414cb6140c
Robert Rothenberg <rrwo@cpan.org>
2004-08-20 23:23:26 +0000

    Import of RRWO/Bundle-Graphics-ColorNames-1.00 from CPAN.
    gitpan-cpan-distribution: Bundle-Graphics-ColorNames
    gitpan-cpan-version:      1.00
    gitpan-cpan-path:         RRWO/Bundle-Graphics-ColorNames-1.00.tar.gz
    gitpan-cpan-author:       RRWO
    gitpan-cpan-maturity:     released

---
+++ lib/Bundle/Graphics/ColorNames.pm
@@ -0,0 +1,45 @@
+package Bundle::Graphics::ColorNames;
+
+use 5.005;
+use strict;
+
+our $VERSION = '1.00';
+
+1;
+
+__END__
+
+=head1 NAME
+
+Bundle::Graphics::ColorNames - bunndle of schemas for Graphics::ColorNames
+
+=head1 SYNOPSIS
+
+  perl -MCPAN -e 'install Bundle::Graphics::ColorNames'
+
+=head1 DESCRIPTION
+
+This bundle provides a way to load L<Graphics::ColorNames> along with
+additional color schemes.
+
+=head1 CONTENTS
+
+Module::Load - used for dynamic module loading
+
+Graphics::ColorNames - base module and schemas for Graphics::ColorNames
+
+Graphics::ColorNames::Mozilla - Mozilla colors
+
+Graphics::ColorNames::VACCC - VisiBone Anglo-Centric Color Codes
+
+=head1 AUTHOR
+
+Robert Rothenberg <rrwo at cpan.org>
+
+=head1 LICENSE
+
+Copyright (c) 2004 Robert Rothenberg. All rights reserved.
+This program is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself.
+
+=cut
